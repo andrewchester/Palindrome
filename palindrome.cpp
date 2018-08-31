9ca4315e654667cb2d1bb3d72673d3a6aa43ba94
U-BSD\476724 <476724@SHS-10L74C39E.beaverton.k12.or.us>
2018-08-31 09:07:50 -0700

    Completed counting function

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -6,9 +8,10 @@ int countPunctuationAndSpaces(char str[])
 {
   int num = 0;
 
-  for(int i = 0; i < sizeof(str); i++){
+  for(int i = 0; i < strlen(str); i++){
     int charValue = (int)str[i];
-    if((charValue >= 33 && charValue <= 47) || (charValue >= 58 && charValue <= 64) || (charValue >= 91 && charValue <= 96) || (charValue >= 123 && charValue <= 126)){
+    if((charValue >= 32 && charValue <= 47) || (charValue >= 58 && charValue <= 64) || (charValue >= 91 && charValue <= 96) || (charValue >= 123 && charValue <= 126)){
+      cout << "Character identified: " << str[i] << endl;
       num++;
     }
   }
@@ -22,6 +25,8 @@ int main()
   cout << "Enter in your sentence to test: ";
   cin.get(sentence, 80);
 
+  cout << "Sentence entered: " << sentence << endl;
+  
   cout << "Number of spaces and punctuation: " << countPunctuationAndSpaces(sentence) << endl;
   return 0;
 }
