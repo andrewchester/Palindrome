7f89ef40030ecb7f068b73ed5b2420e201b0ebff
andrewchester <476724@bsd48.org>
2018-09-04 15:37:57 -0700

    Added comments and finished up  seperateString function

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,22 +1,30 @@
+/*
+*  Andrew Chester
+*  C++ program that takes in a string and tells you if it's a palindrome
+*
+* Used code from: 
+* http://www.cplusplus.com/reference/cstring/strtok/
+*/
+
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
 
 using namespace std;
 
+//Function for removing spaces/puntuation, required a lot of fiddling to avoid segmentation fault errors
 char * seperateString(char * str)
 {
-  char * strsubstring;
-  char newSentence[80];
-  strsubstring = strtok (str," ,.-");
-  while (strsubstring != NULL)
+  char * substring; //Variable for the current substring
+  char * newStr; //Initialize a pointer to a char array
+  newStr = new char[80]; //Initialize that char array to avoid segmentation fault error by accessing memory you haven't allocated
+  substring = strtok (str," ,.-!/(){};:"); //Split the string into tokens using common punctuation/characters as deliminators
+  while (substring != NULL) //Continue looping through substring until all tokens have been seperated
   {
-    strsubstring = strtok (NULL, " ,.-");
-    strcat(newSentence, strsubstring);
+    strcat(newStr, substring); //Concatinate all the tokens onto newStr so that we get 1 cstring at the end
+    substring = strtok (NULL, " ,.-!/(){};:");
   }
-
-  str = newSentence;
-  return str;
+  return newStr; 
 }
 
 int main()
@@ -24,14 +32,15 @@ int main()
   char sentence[80];
   
   cout << "Enter in your sentence to test: ";
-  cin.get(sentence, 80);
+  cin.get(sentence, 79); //Getting user input up to 79 characters then inserting null character(not sure if it automatically inserts it or not)
+  sentence[80] = '\0';
 
-  cin.clear();
-  cin.ignore(100, '\n');
+  cin.clear(); //Clearing the input stream
+  cin.ignore(100, '\n'); 
   
-  sentence = seperateString(sentence);
+  char * str = seperateString(sentence);
 
-  cout << sentence << endl;
+  cout << str << endl;
 
   return 0;
 }
